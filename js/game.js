cd6c9b526ab1fe567a50fe7889e5e0dd77639332
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2016-12-04 21:20:29 +0100

    Added eraser and created a toolbox

--- js/game.js
+++ js/game.js
@@ -18,9 +18,12 @@ var currentColorSprite;
 
 var paletteButtons;
 
-var isPaletteOpen = false;
+var toolButtons;
+
+var isToolboxOpen = false;
 
 function preload() {
+    game.load.image('eraser', 'assets/eraser.png');
 }
 
 function render() {
@@ -30,7 +33,6 @@ function update() {
 }
 
 function create() {
-
     bmd = game.make.bitmapData(window.innerWidth, window.innerHeight);
     bmd.addToWorld();
 
@@ -39,10 +41,16 @@ function create() {
 
     game.input.addMoveCallback(paint, this);
 
+
+    toolButtons = game.add.group();
+    var eraser = game.add.sprite(0, 0, 'eraser');
+    eraser.inputEnabled = true;
+    eraser.events.onInputDown.add(selectTool, this);
+    toolButtons.add(eraser);
+
     drawPalette();
 
-    // //Animate the paletteButtons
-    closePalette();
+    closeToolbox();
 }
 
 function drawPalette() {
@@ -66,47 +74,75 @@ function drawPalette() {
     }
 
     paletteButtons.callAll('events.onInputDown.add', 'events.onInputDown', chooseColor);
-    drawCurrentColorSprite(); 
+    drawSelectedToolSprite(); 
 }
 
-function openPalette() {
+function openToolbox() {
+
     for(var i = 0; i < paletteButtons.children.length; i++) {
         game.add.tween(paletteButtons.children[i]).to( { y: (i * 60) + 60 }, 500, Phaser.Easing.Quadratic.Out, true);
     }
-    isPaletteOpen = true;
+
+    for(var i = 0; i < toolButtons.children.length; i++) {
+        game.add.tween(toolButtons.children[i]).to( { x: (i * 60) + 60 }, 500, Phaser.Easing.Quadratic.Out, true);
+    }
+
+    isToolboxOpen = true;
 }
 
-function closePalette() {
+function closeToolbox() {
     for(var i = 0; i < paletteButtons.children.length; i++) {
         game.add.tween(paletteButtons.children[i]).to( { y: 0}, 500, Phaser.Easing.Quadratic.Out, true);
     }
-    isPaletteOpen = false;
+
+    for(var i = 0; i < toolButtons.children.length; i++) {
+        game.add.tween(toolButtons.children[i]).to( { x: 0}, 500, Phaser.Easing.Quadratic.Out, true);
+    }
+
+    isToolboxOpen = false
 }
 
-function togglePalette() {
-    if(isPaletteOpen){
-        closePalette();
+function toggleToolbox() {
+    if(isToolboxOpen){
+        closeToolbox();
     }
     else {
-        openPalette();
+        openToolbox();
     }
 }
 
-function drawCurrentColorSprite() {
-    //Draw the current color sprite
-    var graphics = game.add.graphics(0, 0);
-    graphics.beginFill(0xFFFFFF, 1);
-    graphics.drawRect(-2, -2, 54, 54);
-    graphics.beginFill(availableColors[currentColorIndex].hex, 1);
-    graphics.drawRect(0, 0, 50, 50);
-    graphics.endFill();
+function drawSelectedToolSprite() {
 
-    currentColorSprite = game.add.sprite(0, 0, graphics.generateTexture());
+    if(currentColorIndex !== -1) {
+        //Draw the current color sprite
+        var graphics = game.add.graphics(0, 0);
+        graphics.beginFill(0xFFFFFF, 1);
+        graphics.drawRect(-1, -1, 52, 52);
+        graphics.beginFill(availableColors[currentColorIndex].hex, 1);
+        graphics.drawRect(0, 0, 50, 50);
+        graphics.endFill();
 
-    currentColorSprite.inputEnabled = true;
-    currentColorSprite.events.onInputDown.add(togglePalette, this);
+        currentColorSprite = game.add.sprite(0, 0, graphics.generateTexture());
 
-    graphics.destroy();
+        currentColorSprite.inputEnabled = true;
+        currentColorSprite.events.onInputDown.add(toggleToolbox, this);
+
+        graphics.destroy();
+    }
+    else //Draw eraser, since thats the only selectable tool we got for now
+    {
+        //Draw the current color sprite
+        var graphics = game.add.graphics(1, 1);
+        graphics.beginFill(0xFFFFFF, 1);
+        graphics.drawRect(-1, -1, 52, 52);
+        graphics.beginFill(0x000000, 1);
+        graphics.drawRect(0, 0, 50, 50);
+        graphics.endFill();
+
+        currentColorSprite = game.add.sprite(1, 1, 'eraser');
+        currentColorSprite.inputEnabled = true;
+        currentColorSprite.events.onInputDown.add(toggleToolbox, this);
+    }
 }
 
 function chooseColor(button) {
@@ -114,8 +150,20 @@ function chooseColor(button) {
     bmd2.circle(16, 16, 16, availableColors[button.colorId].rgba);
 
     currentColorIndex = button.colorId;
-    drawCurrentColorSprite();
-    closePalette();
+    drawSelectedToolSprite();
+    closeToolbox();
+}
+
+function selectTool(button){
+    if(button.key === "eraser"){
+        bmd2 = game.make.bitmapData(128, 128);
+        bmd2.circle(32, 32, 32, 'rgba(0, 0, 0, 1)');
+
+        currentColorIndex = -1;
+        drawSelectedToolSprite();
+    }
+
+    closeToolbox();
 }
 
 function paint(pointer, x, y) {
@@ -130,6 +178,12 @@ function paint(pointer, x, y) {
             }
         }, this);
 
+        toolButtons.forEach(function(button) {
+            if(button.getBounds().contains(pointer.x, pointer.y)){
+               pointerOverAnyButton = true;
+            }
+        }, this);
+
         if(currentColorSprite.input.pointerOver())
         {
             pointerOverAnyButton = true;
@@ -137,6 +191,8 @@ function paint(pointer, x, y) {
 
         if(!pointerOverAnyButton){
             bmd.draw(bmd2, x - 16, y - 16);
+
+            closeToolbox();
         }
     }
 }
